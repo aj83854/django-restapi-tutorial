0df7760eaaccd8f06c0d49d36c36b81f2be87c22
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-17 14:22:56 -0500

    Added login ability to the browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,3 +3,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
