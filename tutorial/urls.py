31284ab26adbaaccb54dda103078486300f8672a
aj83854 <36650362+aj83854@users.noreply.github.com>
2018-09-17 16:35:24 -0500

    Added autogenerated schema view; tutorial complete

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Code Pastebin API')
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
+    url(r'^schema/$', schema_view),
 ]
 
 urlpatterns += [
